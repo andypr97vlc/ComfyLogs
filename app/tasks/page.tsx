606f5f6e1c35df503a794395cca79ee6d2ea09d0
Andy <andypr97vlc@gmail.com>
2025-06-22 14:45:57 +0200

    Solucionado problema de compilacion, eliminado tasks

--- app/tasks/page.tsx
+++
@@ -1,8 +0,0 @@
-// import { createClient } from '@/utils/supabase/server'
-//
-// export default async function Page() {
-//     const supabase = await createClient()
-//     const { data: tasks } = await supabase.from('tasks').select()
-//
-//     return <pre>{JSON.stringify(tasks, null, 2)}</pre>
-// }
